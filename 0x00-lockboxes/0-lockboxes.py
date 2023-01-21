c49d374a02e898ed552f01dd872ebf0ae0bfff5b
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-01-21 13:39:44 -0600

    adding code

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+def canUnlockAll(boxes):
+    ''' Will return true if all boxes can be opened. If not it wil return false '''
+    avKeys = [0]
+    # Loop through availableKeys adding new keys as we go
+    for x in avKeys:
+        for key in boxes[x]:
+            # Append key if it is within boxes range and not yet obtained
+            if key not in avKeys and key < len(boxes):
+                avKeys.append(key)
+    
+    x = 0
+    while x < len(boxes):
+        if x not in avKeys:
+            return False
+        x += 1
