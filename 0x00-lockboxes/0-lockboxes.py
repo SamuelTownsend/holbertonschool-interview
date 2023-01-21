c1821160024329650e3029ba623b5d25bf8a700c
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-01-21 13:49:18 -0600

    adding corrections

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-''' Module woks to see if a set of boxes can all be opened '''
+''' This module attempts to solve the Lockboxes problem '''
+
+
 def canUnlockAll(boxes):
-    ''' Will return true if all boxes can be opened. If not it wil return false '''
-    avKeys = [0]
+    ''' Returns True if all boxes can be opened, otherwise False '''
+    availableKeys = [0]
     # Loop through availableKeys adding new keys as we go
-    for x in avKeys:
+    for x in availableKeys:
         for key in boxes[x]:
             # Append key if it is within boxes range and not yet obtained
-            if key not in avKeys and key < len(boxes):
-                avKeys.append(key)
-    
+            if key not in availableKeys and key < len(boxes):
+                availableKeys.append(key)
+
+    # If there are any numbers missing from availableKeys
+    # between 0 and length of boxes, return false
     x = 0
     while x < len(boxes):
-        if x not in avKeys:
+        if x not in availableKeys:
             return False
         x += 1
+
+    return True
